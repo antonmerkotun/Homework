728459bbc33117e14fb21aaeaec3eb01bff79f79
antonmerkotun <antonmerkotun@gmail.com>
2021-07-01 22:20:45 +0300

    HW2-edit_break

--- javascript/homework2_find-multiples/script.js
+++ javascript/homework2_find-multiples/script.js
@@ -1,12 +1,12 @@
 "use strict";
 
 let number = +prompt(`Введите число`);
-console.log(number)
 
 for (let i = 0; i <= number; i++) {
     if (i % 5 === 0) {
         console.log(i)
-    } else if (number === 1 || number === 2 || number === 3 || number === 4){
+    } else if (number <= 5){
         console.log(`Sorry, no numbers`)
+        break;
     }
 }
