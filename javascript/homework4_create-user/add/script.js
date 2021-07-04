24cd14853a8523a87c3bdc649855a38b8f31c28c
antonmerkotun <antonmerkotun@gmail.com>
2021-07-04 22:07:21 +0300

    hw-4

--- javascript/homework4_create-user/add/script.js
+++ javascript/homework4_create-user/add/script.js
@@ -1,39 +1,18 @@
 "use strict"
 
-                                        // first option
+function createNewUser() {
+    return {
+        firstName: prompt(`Введите имя`),
+        lastName: prompt(`Введите фамилию`),
+        getLogin() {
+            let res = this.firstName[0].toLowerCase() + this.lastName.toLowerCase();
+            console.log(res);
+        },
+    };
+}
 
-
-// function createNewUser() {
-//     let newUser = {
-//         name: prompt(`Введите имя`),
-//         lastName: prompt(`Введите фамилию`),
-//     };
-//     newUser.getLogin = function() {
-//         let res = this.name[0].toLowerCase() + this.lastName.toLowerCase();
-//         console.log(res);
-//     }
-//
-// }
-//
-// createNewUser().getLogin();
-
-
-                                    // option two
-
-// function createNewUser() {
-//     return {
-//         name: prompt(`Введите имя`),
-//         lastName: prompt(`Введите фамилию`),
-//         getLogin() {
-//             let res = this.name[0].toLowerCase() + this.lastName.toLowerCase();
-//             console.log(res);
-//         },
-//
-//     };
-// }
-//
-// const newUser = createNewUser();
-// newUser.getLogin();
+const newUser = createNewUser();
+newUser.getLogin();
 
 
 
