615f2ff3e50c86815270c7d9bf29bcb9ea3a7170
antonmerkotun <antonmerkotun@gmail.com>
2021-07-08 08:49:29 +0300

    edit-5_age

--- javascript/homework5_user-age-and-password /script/script.js
+++ javascript/homework5_user-age-and-password /script/script.js
@@ -2,18 +2,44 @@
 
 function createNewUser() {
     return {
-        firstName: prompt(`Введите имя`),
-        lastName: prompt(`Введите фамилию`),
+        firstName: 'Антон',
+        lastName: 'Меркотун',
         getLogin() {
             let res = this.firstName[0].toLowerCase() + this.lastName.toLowerCase();
             console.log(res);
         },
-        birthday: prompt(`Введите дату рождения`),
+        birthday: `20.06.1995`,
+        getAge(){
+            let now = new Date();
+            let years = this.birthday[6] + this.birthday[7] + this.birthday[8] + this.birthday[9];
+            let month = this.birthday[3] + this.birthday[4];
+            let day = this.birthday[0] + this.birthday[1];
+            let birthday = new Date(years,month,day); //Дата рождения
+            // let age = now.getFullYear() - birthday.getFullYear()
+            return  now.getFullYear() - birthday.getFullYear()
+            // console.log(age)
+        },
+        getPassword (){
+            let pass = this.firstName[0].toUpperCase() + this.lastName.toLowerCase() + this.getAge()
+            console.log(pass)
+        }
     };
 }
 
-
-
-
 const newUser = createNewUser();
 newUser.getLogin();
+newUser.getAge();
+newUser.getPassword();
+
+
+
+// document.write (`Возраст: ${age}`);
+
+// +prompt(`Введите вашу дату рождения`,`dd.mm.yyyy`)
+
+// 20.06.1995
+
+// - this.birthday.slice()
+
+
+// console.log(this.birthday[0] + this.birthday[1] + this.birthday[3] + this.birthday[4] + this.birthday[6] + this.birthday[7] + this.birthday[8] + this.birthday[9])
