f7bc47f866ba482b7b6721709e4ff153dedb3d1f
antonmerkotun <antonmerkotun@gmail.com>
2021-07-11 23:20:26 +0300

    wh6-option_2

--- javascript/homework6_filter-array/script/script.js
+++ javascript/homework6_filter-array/script/script.js
@@ -1,11 +1,30 @@
 'use strict'
 
-function filterBy(array, type){
-    const arrayType = array.filter(function(array) {
-        return typeof array !== type;
-    });
-    console.log(arrayType)
+// 1 //
 
+function filterBy(array, type){
+    const arrayType = array.filter(array => typeof array !== type)
+    console.log(arrayType)
 }
 
 filterBy(['hello', 'world', 23, '23', null], `string`)
+
+
+
+// 2 //
+
+// function filterBy(array, type){
+//     const arrayType = array.filter(function(array) {
+//         return typeof array !== type;
+//     });
+//     console.log(arrayType)
+//
+// }
+//
+// filterBy(['hello', 'world', 23, '23', null], `string`)
+
+
+
+
+
+
