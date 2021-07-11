a8c549a00a031d4b7dce103681e1ad4338b1db4d
antonmerkotun <antonmerkotun@gmail.com>
2021-07-11 22:00:14 +0300

    hw6-done

--- javascript/homework6_filter-array/script/script.js
+++ javascript/homework6_filter-array/script/script.js
@@ -1,1 +1,11 @@
 'use strict'
+
+function filterBy(array, type){
+    const arrayType = array.filter(function(array) {
+        return typeof array !== type;
+    });
+    console.log(arrayType)
+
+}
+
+filterBy(['hello', 'world', 23, '23', null], `string`)
