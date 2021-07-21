f5bcc085ec2505e3dec844e60db647dc73c93e96
antonmerkotun <antonmerkotun@gmail.com>
2021-07-22 00:51:33 +0300

    hw_9-done

--- javascript/homework9_tabs/script/script.js
+++ javascript/homework9_tabs/script/script.js
@@ -1,19 +1,50 @@
 'use strict'
 
-const divCentered = document.querySelector('div')
-console.log(divCentered)
+function tab() {
+    let liTitle = document.querySelectorAll('.tabs-title')
+    let liText = document.querySelectorAll('.tabs-text')
+    let tabName;
 
-const ulTabs = document.querySelector('ul')
-console.log(ulTabs)
+    liTitle.forEach(item => {
+        item.addEventListener('click', selectLiTitle)
+    })
+    function selectLiTitle() {
+        liTitle.forEach(item => {
+            item.classList.remove(`active-title`)
+        })
+        this.classList.add(`active-title`)
+        tabName = this.getAttribute(`datatype`)
+        selectLiText(tabName)
+    }
 
-const li = ulTabs.querySelector('li')
-console.log(li)
+    function selectLiText(tabName) {
+        liText.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add(`active-text`) : item.classList.remove(`active-text`)
+        })
+    }
+}
 
-const ulTabsContent = document.getElementsByClassName('tabs-content')
-console.log(ulTabsContent)
 
 
 
+
+
+
+
+
+
+
+
+
+tab()
+
+
+
+
+
+// li.addEventListener('mouseout', (event) => {
+//
+// });
 
 
 
