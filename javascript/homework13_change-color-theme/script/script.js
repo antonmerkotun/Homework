4606371eceac1fb08229fb4cfa0ca9b1b1b67239
antonmerkotun <antonmerkotun@gmail.com>
2021-07-27 20:48:32 +0300

    hw 13 function - done sessionStorage - process

--- javascript/homework13_change-color-theme/script/script.js
+++ javascript/homework13_change-color-theme/script/script.js
@@ -1,22 +1,27 @@
 'use strict'
 
+const style = document.getElementById('theme_css');
+const theme = localStorage.getItem('theme') || 'light';
+
+style.classList.add(theme);
 
 const selectTheme = () => {
-
     const check = document.getElementById('theme_css').classList[0] === 'light';
-    const style = document.getElementById('theme_css');
     if (check) {
         style.href = './style/style2.css';
         style.classList.remove('light')
         style.classList.add('dark');
+
+        localStorage.setItem('theme', 'dark');
     } else {
         style.href = './style/style.css';
         style.classList.remove('dark')
         style.classList.add('light');
+
+        localStorage.setItem('theme', 'light');
     }
 }
 const button = document.querySelector('.button-theme');
 button.addEventListener('click', selectTheme)
-
 
 
