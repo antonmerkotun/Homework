12586a7fbf8a5c495f8a66afe9d0822c62368bf7
Anton Merkotun <antonmerkotun@gmail.com>
2021-09-10 23:42:12 +0300

    hw1 done

--- advanced-js/homework1-es6-classes/script/script.js
+++ advanced-js/homework1-es6-classes/script/script.js
@@ -1,41 +1,45 @@
 class Employee {
-	constructor(name, age, salary) {
-		this.name = name;
-		this.age = age;
-		this.salary = salary;
+    constructor(name, age, salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    set name(nameValue) {
+        this._name = nameValue;
+    }
+	get	name(){
+		return this._name;
 	}
-
-	set(name, age, salary) {
-		// this._name = name;
-		// this._age = age;
-		// this._salary = salary;
-		// console.log(this._name)
-		// console.log(this._age)
-		// console.log(this._salary)
+	set age(ageValue){
+		this._age = ageValue;
 	}
-
-	get(name, age, salary) {
-
+	get age(){
+		return this._age;
+	}
+	set salary(salaryValue){
+		this._salary = salaryValue;
+	}
+	get salary(){
+		return this._salary;
 	}
 }
 
 class Programmer extends Employee {
-	constructor(name, age, salary, lang) {
-		super(name, age, salary);
-		this.lang = lang;
+    constructor(name, age, salary, lang) {
+        super(name, age, salary);
+        this.lang = lang;
+    }
+	set salary(salaryValue){
+		this._salary = salaryValue;
 	}
-
-	set(salary) {
-
-	}
-
-	get(salary) {
-
+	get	salary(){
+		return this._salary * 3;
 	}
 }
 
 
-const programmer = new Programmer('Uasya', 100, 999999, 'en')
-const programmer2 = new Programmer('Peter', 101, 1000000, 'ua')
+const programmer = new Programmer('Uasya', 44, 100, 'en')
+const programmer2 = new Programmer('Peter', 32, 200, 'ua')
 console.log(programmer)
 console.log(programmer2)
