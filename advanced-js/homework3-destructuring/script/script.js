511d9695d9e34ed7e881063b4c05a23312ae51a9
Anton Merkotun <antonmerkotun@gmail.com>
2021-09-15 21:16:54 +0300

    task-1

--- advanced-js/homework3-destructuring/script/script.js
+++ advanced-js/homework3-destructuring/script/script.js
@@ -4,23 +4,7 @@
 
 const clients1 = ["Гилберт", "Сальваторе", "Пирс", "Соммерс", "Форбс", "Донован", "Беннет"];
 const clients2 = ["Пирс", "Зальцман", "Сальваторе", "Майклсон"];
-
-
-class Clients {
-	constructor(clients1, clients2) {
-		this.clients1 = clients1;
-		this.clients2 = clients2;
-	};
-	checkClients(){
-		let ccc = clients1.forEach(function (ele) {
-				return ele
-		})
-		console.log(ccc)
-
-	}
-}
-
-
-
-const clients = new Clients(clients1, clients2);
-clients.checkClients()
+const [name, name2, name3, name4, name5, name6, name7] = clients1;
+const [,name8, , name9] = clients2;
+const clients = [name, name2, name3, name4, name5, name6, name7].concat([,name8, , name9]);
+console.log(clients);
