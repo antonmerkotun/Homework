54ffa296f60542732f42738858f3e71b198097f7
Anton <antonmerkotun@gmail.com>
2021-09-15 15:37:50 +0300

    correct fn

--- advanced-js/homework3-destructuring/script/script.js
+++ advanced-js/homework3-destructuring/script/script.js
@@ -1,1 +1,26 @@
 'use strict'
+
+// TASK 1
+
+const clients1 = ["Гилберт", "Сальваторе", "Пирс", "Соммерс", "Форбс", "Донован", "Беннет"];
+const clients2 = ["Пирс", "Зальцман", "Сальваторе", "Майклсон"];
+
+
+class Clients {
+	constructor(clients1, clients2) {
+		this.clients1 = clients1;
+		this.clients2 = clients2;
+	};
+	checkClients(){
+		let ccc = clients1.forEach(function (ele) {
+				return ele
+		})
+		console.log(ccc)
+
+	}
+}
+
+
+
+const clients = new Clients(clients1, clients2);
+clients.checkClients()
