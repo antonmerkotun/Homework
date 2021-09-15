5a00f32e4b6bf221bf388b050314243bfdf5799a
Anton Merkotun <antonmerkotun@gmail.com>
2021-09-16 00:09:57 +0300

    hw3 done

--- advanced-js/homework3-destructuring/script/script.js
+++ advanced-js/homework3-destructuring/script/script.js
@@ -1,10 +1,160 @@
 'use strict'
 
+
 // TASK 1
 
-const clients1 = ["Гилберт", "Сальваторе", "Пирс", "Соммерс", "Форбс", "Донован", "Беннет"];
-const clients2 = ["Пирс", "Зальцман", "Сальваторе", "Майклсон"];
-const [name, name2, name3, name4, name5, name6, name7] = clients1;
-const [,name8, , name9] = clients2;
-const clients = [name, name2, name3, name4, name5, name6, name7].concat([,name8, , name9]);
-console.log(clients);
+
+// const clients1 = ["Гилберт", "Сальваторе", "Пирс", "Соммерс", "Форбс", "Донован", "Беннет"];
+// const clients2 = ["Пирс", "Зальцман", "Сальваторе", "Майклсон"];
+// const mySet2 = new Set([...clients1, ...clients2]);
+// console.log(mySet2);
+
+
+// TASK 2
+
+
+// const characters = [
+//     {
+//         name: "Елена",
+//         lastName: "Гилберт",
+//         age: 17,
+//         gender: "woman",
+//         status: "human"
+//     },
+//     {
+//         name: "Кэролайн",
+//         lastName: "Форбс",
+//         age: 17,
+//         gender: "woman",
+//         status: "human"
+//     },
+//     {
+//         name: "Аларик",
+//         lastName: "Зальцман",
+//         age: 31,
+//         gender: "man",
+//         status: "human"
+//     },
+//     {
+//         name: "Дэймон",
+//         lastName: "Сальваторе",
+//         age: 156,
+//         gender: "man",
+//         status: "vampire"
+//     },
+//     {
+//         name: "Ребекка",
+//         lastName: "Майклсон",
+//         age: 1089,
+//         gender: "woman",
+//         status: "vempire"
+//     },
+//     {
+//         name: "Клаус",
+//         lastName: "Майклсон",
+//         age: 1093,
+//         gender: "man",
+//         status: "vampire"
+//     }
+// ];
+// //
+// const user = characters.map(function (el) {
+//     const {name, lastName, age} = el
+//     return {name, lastName, age};
+// })
+// console.log(user);
+
+
+// TASK 3
+
+
+// const user1 = {
+//     name: "John",
+//     years: 30
+// };
+// const {name, years: age, isAdmin = false} = user1;
+// console.log(name);
+// console.log(age);
+// console.log(isAdmin);
+
+
+// TASK 4
+
+
+// const satoshi2020 = {
+//     name: 'Nick',
+//     surname: 'Sabo',
+//     age: 51,
+//     country: 'Japan',
+//     birth: '1979-08-21',
+//     location: {
+//         lat: 38.869422,
+//         lng: 139.876632
+//     }
+// }
+//
+// const satoshi2019 = {
+//     name: 'Dorian',
+//     surname: 'Nakamoto',
+//     age: 44,
+//     hidden: true,
+//     country: 'USA',
+//     wallet: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+//     browser: 'Chrome'
+// }
+//
+// const satoshi2018 = {
+//     name: 'Satoshi',
+//     surname: 'Nakamoto',
+//     technology: 'Bitcoin',
+//     country: 'Japan',
+//     browser: 'Tor',
+//     birth: '1975-04-05'
+// }
+// let fullProfile = {...satoshi2018, ...satoshi2019, ...satoshi2020};
+// console.log(fullProfile);
+
+
+// TASK 5
+
+
+// const books = [{
+//     name: 'Harry Potter',
+//     author: 'J.K. Rowling'
+// }, {
+//     name: 'Lord of the rings',
+//     author: 'J.R.R. Tolkien'
+// }, {
+//     name: 'The witcher',
+//     author: 'Andrzej Sapkowski'
+// }];
+//
+// const bookToAdd = {
+//     name: 'Game of thrones',
+//     author: 'George R. R. Martin'
+// }
+//
+// books.push(bookToAdd);
+// console.log(books);
+
+
+// TASK 6
+
+
+// const employee = {
+//     name: 'Vitalii',
+//     surname: 'Klichko'
+// }
+// const user = new Object(employee);
+// const {name, surname, age = 41, salary = 10000} = user;
+// const vitalii = {name, surname, age, salary};
+// console.log(vitalii);
+
+
+// TASK 7
+
+
+// const array = ['value', () => 'showValue'];
+// const [value, showValue] = array
+// alert(value);
+// alert(showValue());
