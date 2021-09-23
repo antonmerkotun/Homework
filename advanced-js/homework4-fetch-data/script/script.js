ebe3a90b1225b529b7b2332c4e041eb11173e3de
Anton <antonmerkotun@gmail.com>
2021-09-23 16:12:08 +0300

    start hw4

--- advanced-js/homework4-fetch-data/script/script.js
+++ advanced-js/homework4-fetch-data/script/script.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const url = 'https://ajax.test-danit.com/api/swapi/films';
+
+fetch(url)
+	.then(response => {
+		return response.json()
+	})
+	.then(array => {
+		return array.forEach(ele => {
+			const {id, characters, director, episodeId, openingCrawl} = ele
+			console.log(ele)
+			let li = document.createElement('li')
+			li.textContent = ele.name
+			document.body.append(li)
+		})
+	})
+
+
+
