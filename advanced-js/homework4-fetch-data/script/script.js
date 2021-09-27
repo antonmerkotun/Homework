dbac8aa68865cc052c2160e4bfe6cce630729c36
Anton Merkotun <antonmerkotun@gmail.com>
2021-09-27 08:50:29 +0300

    hw 4

--- advanced-js/homework4-fetch-data/script/script.js
+++ advanced-js/homework4-fetch-data/script/script.js
@@ -1,20 +1,57 @@
 'use strict'
 
+class Films {
+    constructor(url) {
+        this.url = url;
+    }
+
+    getFilms() {
+        let form = document.createElement('div');
+        axios
+            .get(this.url)
+            .then(function ({data}) {
+                data.forEach(function ({characters}) {
+                    console.log(characters)
+                })
+                // for (let key in data) {
+                //     let li = document.createElement('li');
+                //     li.textContent = key.name;
+                //     document.body.append(li);
+                // console.log(data)
+
+                // }
+            })
+
+    }
+
+    // getCharacters(characters) {
+    //     return fetch('https://ajax.test-danit.com/api/swapi/films/characters')
+    //         .then(response => {
+    //             return response.json();
+    //         })
+    //         .then(character => {
+    //             console.log(character)
+    //         })
+    // }
+
+}
+
+// fetch(url)
+// 	.then(response => {
+// 		return response.json()
+// 	})
+// 	.then(array => {
+// 		return array.forEach(ele => {
+// 			let li = document.createElement('li')
+// 			li.textContent = ele.name
+// 			document.body.append(li)
+// 			// console.log(ele.characters)
+// 		})
+//
+// 	})
+
+
 const url = 'https://ajax.test-danit.com/api/swapi/films';
-
-fetch(url)
-	.then(response => {
-		return response.json()
-	})
-	.then(array => {
-		return array.forEach(ele => {
-			const {id, characters, director, episodeId, openingCrawl} = ele
-			console.log(ele)
-			let li = document.createElement('li')
-			li.textContent = ele.name
-			document.body.append(li)
-		})
-	})
-
-
-
+const films = new Films(url)
+films.getFilms()
+// films.getCharacters()
