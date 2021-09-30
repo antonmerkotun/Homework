568b3ccd1eb2843d3434180cbbdfa5abd796877b
Anton Merkotun <antonmerkotun@gmail.com>
2021-09-30 23:42:30 +0300

    class Card

--- advanced-js/homework5_twitter/script/script.js
+++ advanced-js/homework5_twitter/script/script.js
@@ -3,64 +3,53 @@
 const urlUsers = "https://ajax.test-danit.com/api/json/users"
 const urlPosts = "https://ajax.test-danit.com/api/json/posts"
 
-class Card {
+class List {
     constructor(urlUsers, urlPosts) {
         this.urlUsers = urlUsers
         this.urlPosts = urlPosts
-    }
-
-    getListPosts() {
-        return axios
-            .get(this.urlPosts)
-            .then(function ({data}) {
-                // console.log(data)
-
-
-                data.forEach(ele => {
-                    const form = document.createElement('form')
-                    const body = document.createElement('p')
-                    const title = document.createElement('p')
-                    document.body.append(form)
-                    form.append(body, title)
-                    body.textContent = ele.body
-                    title.textContent = ele.title
-                    form.style.width = '500px'
-                    form.style.paddingTop = '100px'
-                    form.style.margin = '0 auto'
-
-                })
-
-
-
-            })
     }
 
     getListUsers() {
         return axios
             .get(this.urlUsers)
             .then(function ({data}) {
-                // console.log(data)
+                return data
+            })
+    }
 
-
-                // data.forEach(el => {
-                    // console.log(el)
-                    // const form = document.createElement('form')
-                    // const name = document.createElement('p')
-                    // const email = document.createElement('p')
-                    // document.body.append(form)
-                    // form.append(name, email)
-                    // name.textContent = el.name
-                    // email.textContent = el.email
-                // })
-
-
-
+    getListPosts() {
+        return axios
+            .get(this.urlPosts)
+            .then(function ({data}) {
+                return data
             })
     }
 }
 
+class Card {
+    constructor(users, posts) {
+        this.users = users
+        this.posts = posts
+    }
 
-const card = new Card(urlUsers, urlPosts);
+    form() {
+        console.log(this.users)
+        console.log(this.posts)
 
-card.getListUsers()
-card.getListPosts()
+        const form = document.createElement('form')
+        const body = document.createElement('p')
+        const title = document.createElement('p')
+        document.body.append(form)
+        form.append(body, title)
+        // body.textContent = ele.body
+        // title.textContent = ele.title
+        form.style.width = '500px'
+        form.style.paddingTop = '100px'
+        form.style.margin = '0 auto'
+    }
+}
+
+const list = new List(urlUsers, urlPosts);
+const card = new Card(list.getListUsers(), list.getListPosts())
+
+card.form()
