3080f126a7f87b7e21aa94a1cf5424aa195bce18
Anton <antonmerkotun@gmail.com>
2021-10-01 16:25:18 +0300

    class list

--- advanced-js/homework5_twitter/script/script.js
+++ advanced-js/homework5_twitter/script/script.js
@@ -3,53 +3,59 @@
 const urlUsers = "https://ajax.test-danit.com/api/json/users"
 const urlPosts = "https://ajax.test-danit.com/api/json/posts"
 
+class App {
+	constructor(urlUsers, urlPosts) {
+		this.urlUsers = urlUsers
+		this.urlPosts = urlPosts
+	}
+
+	getListUsers() {
+		return axios
+			.get(this.urlUsers)
+			.then(function ({data}) {
+				return data
+			})
+	}
+
+	getListPosts() {
+		return axios
+			.get(this.urlPosts)
+			.then(function ({data}) {
+				return data
+			})
+	}
+}
+
+
 class List {
-    constructor(urlUsers, urlPosts) {
-        this.urlUsers = urlUsers
-        this.urlPosts = urlPosts
-    }
+	constructor(name, email, title, body) {
+		this.name = name
+		this.email = email
+		this.title = title
+		this.body = body
+	}
 
-    getListUsers() {
-        return axios
-            .get(this.urlUsers)
-            .then(function ({data}) {
-                return data
-            })
-    }
-
-    getListPosts() {
-        return axios
-            .get(this.urlPosts)
-            .then(function ({data}) {
-                return data
-            })
-    }
 }
 
 class Card {
-    constructor(users, posts) {
-        this.users = users
-        this.posts = posts
-    }
+	constructor() {
 
-    form() {
-        console.log(this.users)
-        console.log(this.posts)
+	}
 
-        const form = document.createElement('form')
-        const body = document.createElement('p')
-        const title = document.createElement('p')
-        document.body.append(form)
-        form.append(body, title)
-        // body.textContent = ele.body
-        // title.textContent = ele.title
-        form.style.width = '500px'
-        form.style.paddingTop = '100px'
-        form.style.margin = '0 auto'
-    }
+	form() {
+		// console.log(this.users)
+		// console.log(this.posts)
+		// const form = document.createElement('form')
+		// const body = document.createElement('p')
+		// const title = document.createElement('p')
+		// document.body.append(form)
+		// form.append(body, title)
+		// body.textContent = ele.body
+		// title.textContent = ele.title
+		// form.style.width = '500px'
+		// form.style.paddingTop = '100px'
+		// form.style.margin = '0 auto'
+	}
 }
 
-const list = new List(urlUsers, urlPosts);
-const card = new Card(list.getListUsers(), list.getListPosts())
-
-card.form()
+const app = new App(urlUsers, urlPosts);
