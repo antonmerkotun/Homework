90c868e387b6fdff64adf6e2804e959327d68642
Anton Merkotun <antonmerkotun@gmail.com>
2021-10-02 15:06:13 +0300

    method delete start

--- advanced-js/homework5_twitter/script/script.js
+++ advanced-js/homework5_twitter/script/script.js
@@ -1,65 +1,132 @@
 'use strict'
 
-const urlUsers = "https://ajax.test-danit.com/api/json/users"
-const urlPosts = "https://ajax.test-danit.com/api/json/posts"
+const urlUsers = "https://ajax.test-danit.com/api/json/users/"
+const urlPosts = "https://ajax.test-danit.com/api/json/posts/"
+const cardDelete = "https://ajax.test-danit.com/api/json/posts/${postId}"
 
 class App {
-	constructor(urlUsers, urlPosts) {
-		this.urlUsers = urlUsers
-		this.urlPosts = urlPosts
-	}
+    constructor(urlUsers, urlPosts, cardDelete) {
+        this.urlUsers = urlUsers
+        this.urlPosts = urlPosts
+        this.cardDelete = cardDelete
+    }
 
-	getListUsers() {
-		return axios
-			.get(this.urlUsers)
-			.then(function ({data}) {
-				return data
-			})
-	}
+    getUsers() {
+        return fetch(this.urlUsers, {
+            method: "GET"
+        })
+            .then(response => response.json())
+            .then((data) => {
+                return data
+            })
+    }
 
-	getListPosts() {
-		return axios
-			.get(this.urlPosts)
-			.then(function ({data}) {
-				return data
-			})
-	}
+    getPosts() {
+        return fetch(this.urlPosts, {
+            method: "GET"
+        })
+            .then(response => response.json())
+            .then((data) => {
+                return data
+            })
+    }
+
+    deleteCard() {
+        return fetch(this.cardDelete, {
+            method: "DELETE"
+        })
+            .then(response => response)
+    }
 }
 
 
 class List {
-	constructor(name, email, title, body) {
-		this.name = name
-		this.email = email
-		this.title = title
-		this.body = body
-	}
-		createList() {
-		const root = document.querySelector('#root')
-			const form = document.createElement('form')
-			const body = document.createElement('p')
-			const title = document.createElement('p')
-			document.body.append(form)
-			form.append(body, title)
-			body.textContent = ele.body
-			title.textContent = ele.title
-			form.style.width = '500px'
-			form.style.paddingTop = '100px'
-			form.style.margin = '0 auto'
-		}
+    constructor(name, email, title, body) {
+        this.name = name
+        this.email = email
+        this.title = title
+        this.body = body
+    }
+
+    createList() {
+        const root = document.querySelector('#root')
+
+        const div = document.createElement('div')
+        div.classList.add("card", "container")
+        div.style = "width: 30rem;"
+        div.style.marginTop = "100px"
+
+        const divTitle = document.createElement('div')
+        divTitle.classList.add("card-body")
+
+        const name = document.createElement('h5')
+        name.classList.add("card-title")
+        name.textContent = this.name
+
+        const email = document.createElement('p')
+        email.classList.add("card-text")
+        email.textContent = this.email
+
+        const ul = document.createElement('ul')
+        ul.classList.add("list-group", "list-group-flush")
+
+        const title = document.createElement('li')
+        title.classList.add("list-group-item")
+        title.textContent = this.title
+
+        const body = document.createElement('li')
+        body.classList.add("list-group-item")
+        body.textContent = this.body
+
+        const divButton = document.createElement('div')
+        divButton.classList.add("card-body")
+
+        const button = document.createElement('a')
+        button.classList.add("btn", "btn-primary")
+        button.href = ''
+        button.textContent = 'Delete card'
+        button.style.marginLeft = '300px'
+        button.addEventListener('click', function (e) {
+            e.preventDefault()
+            div.style.display = 'none'
+        })
+
+        root.append(div)
+        div.append(divTitle, ul, divButton)
+        divTitle.append(name, email)
+        ul.append(title, body)
+        divButton.append(button)
+    }
 }
 
 class Card {
-	constructor() {
+    constructor(app, list) {
+        this.app = app
+        this.list = list
+    }
 
-	}
-	form() {
-
-	}
+    form() {
+        this.app.getUsers()
+            .then(function (users) {
+                app.getPosts()
+                    .then(function (post) {
+                        users.forEach(user => {
+                            post.forEach(post => {
+                                if (user.id === post.id) {
+                                    list.name = user.name
+                                    list.email = user.email
+                                    list.title = post.title
+                                    list.body = post.body
+                                    list.createList()
+                                }
+                            })
+                        })
+                    })
+            });
+    }
 }
 
-const app = new App(urlUsers, urlPosts);
-app.getListUsers();
-app.getListPosts();
+const app = new App(urlUsers, urlPosts, cardDelete);
 const list = new List()
-list.createList()
+const card = new Card(app, list)
+card.form()
