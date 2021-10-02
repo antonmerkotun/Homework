12e5c544e7200f73229d81be6fcc0e1a489ce231
Anton Merkotun <antonmerkotun@gmail.com>
2021-10-02 23:40:00 +0300

    hw 6 done

--- advanced-js/homework6-async-await/script/script.js
+++ advanced-js/homework6-async-await/script/script.js
@@ -1,9 +1,6 @@
 'use strict'
 
 const urlIp = "https://api.ipify.org/?format=json"
-const urlApi = "http://ip-api.com/json/"
-const search = "?fields="
-const generatedNumeric = "66846719"
 
 const root = document.querySelector('#root')
 const div = document.createElement('div')
@@ -22,35 +19,37 @@ async function fetchIp() {
     const ip = data.ip
 
     async function fetchApi() {
-        const response = await fetch(`${urlApi}${ip}${search}${generatedNumeric}`)
+        const response = await fetch(`http://ip-api.com/json/${ip}?fields=66846719`)
         const data = await response.json()
 
         const list = document.createElement('ul')
-        list.classList.add("list-group", "container")
-        list.style.marginTop = "50px"
+        list.classList.add("list-group")
+        list.style.margin = "50px auto"
+        list.style.maxWidth = "300px"
         root.append(list)
 
         const continent = document.createElement('li')
-        continent.classList.add("list-group-item")
+        continent.classList.add("list-group-item", "list-group-item-success")
         continent.textContent = data.continent
 
         const country = document.createElement('li')
-        country.classList.add("list-group-item")
+        country.classList.add("list-group-item", "list-group-item-danger")
         country.textContent = data.country
 
         const region = document.createElement('li')
-        region.classList.add("list-group-item")
+        region.classList.add("list-group-item", "list-group-item-warning")
         region.textContent = data.regionName
 
         const city = document.createElement('li')
-        city.classList.add("list-group-item")
+        city.classList.add("list-group-item", "list-group-item-info")
         city.textContent = data.city
 
         const district = document.createElement('li')
-        district.classList.add("list-group-item")
+        district.classList.add("list-group-item", "list-group-item-primary")
         district.textContent = data.district
 
         list.append(continent, country, region, city, district)
+
     }
 
     fetchApi()
