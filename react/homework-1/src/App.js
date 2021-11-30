fa2fc8682f0882650b1175dade073655e1cc69cb
Anton <antonmerkotun@gmail.com>
2021-11-30 15:40:18 +0200

    hw2

--- react/homework-1/src/App.js
+++ react/homework-1/src/App.js
@@ -1,38 +1,46 @@
 import React from "react";
-// import Button from "./components/Button/Button.jsx";
-// import Modal from "./components/Modal/Modal.jsx";
 import "./components/Button/Button.scss"
-// import modalData from "./components/Modal/ModalData";
+import modalData from "./components/Modal/ModalData";
 import Card from "./components/Card/Card.jsx";
-
+import Button from "./components/Button/Button";
+import Modal from "./components/Modal/Modal";
+import "./App.scss"
+import "./components/Card/Card.scss"
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            modalObject: {},
+            modalToShow: "none",
             arrayProduct: [],
         }
     }
 
-    // openModal = (e) => {
-    //     const modalID = e.target.dataset.modalId;
-    //     const modalDeclaration = modalData.find(item => item.id === +modalID);
-    //     this.setState({
-    //         modalObject: {...modalDeclaration},
-    //         modalToShow: "Open modal",
-    //     })
-    // }
-    //
-    // closeModal = (e) => {
-    //     const target = e.target.className
-    //     if (target === "modal" ||
-    //         target === "modal_header-button" ||
-    //         target === "modal_body-buttons-cancel") {
-    //         this.setState({
-    //             modalToShow: "none"
-    //         })
-    //     }
-    // }
+    openModal = (e) => {
+        const modalID = e.target.dataset.modalId;
+        const modalDeclaration = modalData.find(item => item.id === +modalID);
+        this.setState({
+            modalObject: {...modalDeclaration},
+            modalToShow: "Open modal",
+        })
+    }
+
+    closeModal = (e) => {
+        const target = e.target.className
+        if (target === "modal" ||
+            target === "modal_header-button" ||
+            target === "modal_body-buttons-cancel") {
+            this.setState({
+                modalToShow: "none"
+            })
+        }
+    }
+
+    setStar = (e) => {
+        e.target.classList.toggle('icon-color')
+        console.log(e.target.className)
+    }
 
     componentDidMount() {
         fetch("productList.json")
@@ -48,45 +56,38 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className={"product-card"}>
+            <div className="product-list">
                 {this.state.arrayProduct.map(e => (
                         <Card
                             key={e.id}
+                            onClick={this.setStar}
                             article={e.article}
                             src={e.src}
                             productName={e.productName}
                             price={e.price}
                             color={e.color}
+                            button={
+                                <Button
+                                    dataModalId="1"
+                                    className="button-modal"
+                                    backgroundColor={{background: "#FFF"}}
+                                    onClick={this.openModal}
+                                    text="Add to card"
+                                />
+                            }
                         />
                     )
                 )}
+                {this.state.modalToShow === "Open modal" &&
+                    <Modal
+                        onClick={this.closeModal}
+                        header={this.state.modalObject.header}
+                        closeButton={this.state.modalObject.closeButton}
+                        text={this.state.modalObject.text}
+                        action={this.state.modalObject.action}
+                    />
+                }
             </div>
-
-            // <>
-            //     <div className={"buttons"}>
-            //         <Button
-            //             dataModalId={1}
-            //             backgroundColor={{background: "#f8ec02"}}
-            //             text='Open first modal'
-            //             onClick={this.openModal}
-            //         />
-            //         <Button
-            //             dataModalId={2}
-            //             backgroundColor={{background: "#54f802"}}
-            //             text='Open second modal'
-            //             onClick={this.openModal}
-            //         />
-            //     </div>
-            //     {this.state.modalToShow === "Open modal" &&
-            //         <Modal
-            //             onClick={this.closeModal}
-            //             header={this.state.modalObject.header}
-            //             closeButton={this.state.modalObject.closeButton}
-            //             text={this.state.modalObject.text}
-            //             action={this.state.modalObject.action}
-            //         />
-            //     }
-            // </>
         )
     }
 }
