180f6a6a8274a16ebb5c9c8bad97da96b44b4be8
Anton <antonmerkotun@gmail.com>
2021-12-10 14:11:53 +0200

    converting classes into functions

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,150 +9,266 @@ import "./components/ProductCard/ProductCard.scss"
 
 let basketIcon = []
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      arrayProduct: [],
-      modalObject: {},
-      modalToShow: "none",
-      basket: '',
-    }
-  }
 
-  componentDidMount() {
-    fetch("productList.json")
-        .then(response => response.json())
-        .then(result => {
-              this.setState({
-                arrayProduct: result
-              });
-            }
-        )
-    if (basketIcon.length === 0) {
-      basketIcon = JSON.parse(localStorage.getItem("basketIcon"))
-    }
-    if (basketIcon === null) {
-      basketIcon = []
-    }
-  }
+const App = () => {
 
-  openModal = (e) => {
-    const modalID = e.target.dataset.modalId;
-    const modalDeclaration = modalData.find(item => item.id === +modalID);
-    this.setState({
-      modalObject: {...modalDeclaration},
-      modalToShow: "Open modal",
-    })
-    this.state.arrayProduct.forEach(ele => {
-      if (ele.id === +e.target.id) {
+    const componentDidMount = () => {
+        fetch("productList.json")
+            .then(response => response.json())
+            .then(result => {
+                    this.setState({
+                        arrayProduct: result
+                    });
+                }
+            )
+        if (basketIcon.length === 0) {
+            basketIcon = JSON.parse(localStorage.getItem("basketIcon"))
+        }
+        if (basketIcon === null) {
+            basketIcon = []
+        }
+    }
+
+    const openModal = (e) => {
+        const modalID = e.target.dataset.modalId;
+        const modalDeclaration = modalData.find(item => item.id === +modalID);
         this.setState({
-          basket: ele
+            modalObject: {...modalDeclaration},
+            modalToShow: "Open modal",
         })
-      }
-    })
-  }
-
-  closeModal = (e) => {
-    const target = e.target.className
-    if (target === "modal" ||
-        target === "modal_header-button" ||
-        target === "modal_body-buttons-cancel") {
-      this.setState({
-        modalToShow: "none"
-      })
+        this.state.arrayProduct.forEach(ele => {
+            if (ele.id === +e.target.id) {
+                this.setState({
+                    basket: ele
+                })
+            }
+        })
     }
-    if (target === "modal_body-buttons-save") {
-      basketIcon.push(this.state.basket)
-      this.setState({
-        modalToShow: "none"
-      })
-      localStorage.setItem("basketIcon", JSON.stringify(basketIcon))
+
+    const closeModal = (e) => {
+        const target = e.target.className
+        if (target === "modal" ||
+            target === "modal_header-button" ||
+            target === "modal_body-buttons-cancel") {
+            this.setState({
+                modalToShow: "none"
+            })
+        }
+        if (target === "modal_body-buttons-save") {
+            basketIcon.push(this.state.basket)
+            this.setState({
+                modalToShow: "none"
+            })
+            localStorage.setItem("basketIcon", JSON.stringify(basketIcon))
+        }
     }
-  }
 
-  // setFavorite = (e) => {
-  //   e.target.classList.toggle('icon-favorite-add')
-  //   const className = e.target.className
-  //   this.state.arrayProduct.forEach(ele => {
-  //     if (+e.target.id === ele.id) {
-  //       localStorage.setItem(ele.id, className)
-  //     }
-  //   })
-  // }
-
-  setFavorite = (event) => {
-    const el = event.target;
-    const id = el.id
-    if (id) {
-      let favorites = JSON.parse(localStorage.getItem("favorite")) || []
-      if (favorites.includes(id)) {
-        el.classList = 'icon-favorite'
-        favorites = favorites.filter(fId => fId !== id)
-      } else {
-        el.classList = 'icon-favorite-add'
-        favorites.push(id)
-      }
-      localStorage.setItem(el.id, el.className)
-      localStorage.setItem("favorite", JSON.stringify(favorites))
-      this.setState({
-        favorite: favorites
-      })
-      // console.log(JSON.parse(localStorage.getItem('favorite')))
-      // JSON.parse(localStorage.getItem('favorite')).forEach(e => {
-      //     console.log(e)
-      // })
+    const setFavorite = (event) => {
+        const el = event.target;
+        const id = el.id
+        if (id) {
+            let favorites = JSON.parse(localStorage.getItem("favorite")) || []
+            if (favorites.includes(id)) {
+                el.classList = 'icon-favorite'
+                favorites = favorites.filter(fId => fId !== id)
+            } else {
+                el.classList = 'icon-favorite-add'
+                favorites.push(id)
+            }
+            localStorage.setItem(el.id, el.className)
+            localStorage.setItem("favorite", JSON.stringify(favorites))
+            this.setState({
+                favorite: favorites
+            })
+            // console.log(JSON.parse(localStorage.getItem('favorite')))
+            // JSON.parse(localStorage.getItem('favorite')).forEach(e => {
+            //     console.log(e)
+            // })
+        }
     }
-  }
 
-  render() {
     return (
         <>
-          <div className={"header"}>
-            <button className={"button-favorite"}>Favorite</button>
-            <div>
-              <button className={"basket"}/>
-              <span className={"basket-number"}>{basketIcon.length}</span>
+            <div className={"header"}>
+                <button className={"button-favorite"}>Favorite</button>
+                <div>
+                    <button className={"basket"}/>
+                    <span className={"basket-number"}>{basketIcon.length}</span>
+                </div>
             </div>
-          </div>
-          <div className="product-list">
-            {this.state.arrayProduct.map(e => (
-                    <ProductCard
-                        id={e.id}
-                        icon={localStorage.getItem(e.id)}
-                        key={e.id}
-                        onClick={this.setFavorite}
-                        article={e.article}
-                        src={e.src}
-                        productName={e.productName}
-                        price={e.price}
-                        color={e.color}
-                        button={
-                          <Button
-                              id={e.id}
-                              dataModalId="1"
-                              className="button-modal"
-                              backgroundColor={{background: "#FFF"}}
-                              onClick={this.openModal}
-                              text="Add to card"
-                          />
-                        }
+            <div className="product-list">
+                {this.state.arrayProduct.map(e => (
+                        <ProductCard
+                            id={e.id}
+                            icon={localStorage.getItem(e.id)}
+                            key={e.id}
+                            onClick={setFavorite}
+                            article={e.article}
+                            src={e.src}
+                            productName={e.productName}
+                            price={e.price}
+                            color={e.color}
+                            button={
+                                <Button
+                                    id={e.id}
+                                    dataModalId="1"
+                                    className="button-modal"
+                                    backgroundColor={{background: "#FFF"}}
+                                    onClick={openModal}
+                                    text="Add to card"
+                                />
+                            }
+                        />
+                    )
+                )}
+                {this.state.modalToShow === "Open modal" &&
+                    <Modal
+                        onClick={closeModal}
+                        header={this.state.modalObject.header}
+                        closeButton={this.state.modalObject.closeButton}
+                        text={this.state.modalObject.text}
+                        action={this.state.modalObject.action}
                     />
-                )
-            )}
-            {this.state.modalToShow === "Open modal" &&
-                <Modal
-                    onClick={this.closeModal}
-                    header={this.state.modalObject.header}
-                    closeButton={this.state.modalObject.closeButton}
-                    text={this.state.modalObject.text}
-                    action={this.state.modalObject.action}
-                />
-            }
-          </div>
+                }
+            </div>
         </>
     )
-  }
+}
+
+class App2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrayProduct: [],
+            modalObject: {},
+            modalToShow: "none",
+            basket: '',
+        }
+    }
+
+    // componentDidMount() {
+    //     fetch("productList.json")
+    //         .then(response => response.json())
+    //         .then(result => {
+    //                 this.setState({
+    //                     arrayProduct: result
+    //                 });
+    //             }
+    //         )
+    //     if (basketIcon.length === 0) {
+    //         basketIcon = JSON.parse(localStorage.getItem("basketIcon"))
+    //     }
+    //     if (basketIcon === null) {
+    //         basketIcon = []
+    //     }
+    // }
+
+    // openModal = (e) => {
+    //     const modalID = e.target.dataset.modalId;
+    //     const modalDeclaration = modalData.find(item => item.id === +modalID);
+    //     this.setState({
+    //         modalObject: {...modalDeclaration},
+    //         modalToShow: "Open modal",
+    //     })
+    //     this.state.arrayProduct.forEach(ele => {
+    //         if (ele.id === +e.target.id) {
+    //             this.setState({
+    //                 basket: ele
+    //             })
+    //         }
+    //     })
+    // }
+
+    // closeModal = (e) => {
+    //     const target = e.target.className
+    //     if (target === "modal" ||
+    //         target === "modal_header-button" ||
+    //         target === "modal_body-buttons-cancel") {
+    //         this.setState({
+    //             modalToShow: "none"
+    //         })
+    //     }
+    //     if (target === "modal_body-buttons-save") {
+    //         basketIcon.push(this.state.basket)
+    //         this.setState({
+    //             modalToShow: "none"
+    //         })
+    //         localStorage.setItem("basketIcon", JSON.stringify(basketIcon))
+    //     }
+    // }
+
+    // setFavorite = (event) => {
+    //     const el = event.target;
+    //     const id = el.id
+    //     if (id) {
+    //         let favorites = JSON.parse(localStorage.getItem("favorite")) || []
+    //         if (favorites.includes(id)) {
+    //             el.classList = 'icon-favorite'
+    //             favorites = favorites.filter(fId => fId !== id)
+    //         } else {
+    //             el.classList = 'icon-favorite-add'
+    //             favorites.push(id)
+    //         }
+    //         localStorage.setItem(el.id, el.className)
+    //         localStorage.setItem("favorite", JSON.stringify(favorites))
+    //         this.setState({
+    //             favorite: favorites
+    //         })
+    //         // console.log(JSON.parse(localStorage.getItem('favorite')))
+    //         // JSON.parse(localStorage.getItem('favorite')).forEach(e => {
+    //         //     console.log(e)
+    //         // })
+    //     }
+    // }
+
+    // render() {
+    //     return (
+    //         <>
+    //             <div className={"header"}>
+    //                 <button className={"button-favorite"}>Favorite</button>
+    //                 <div>
+    //                     <button className={"basket"}/>
+    //                     <span className={"basket-number"}>{basketIcon.length}</span>
+    //                 </div>
+    //             </div>
+    //             <div className="product-list">
+    //                 {this.state.arrayProduct.map(e => (
+    //                         <ProductCard
+    //                             id={e.id}
+    //                             icon={localStorage.getItem(e.id)}
+    //                             key={e.id}
+    //                             onClick={this.setFavorite}
+    //                             article={e.article}
+    //                             src={e.src}
+    //                             productName={e.productName}
+    //                             price={e.price}
+    //                             color={e.color}
+    //                             button={
+    //                                 <Button
+    //                                     id={e.id}
+    //                                     dataModalId="1"
+    //                                     className="button-modal"
+    //                                     backgroundColor={{background: "#FFF"}}
+    //                                     onClick={this.openModal}
+    //                                     text="Add to card"
+    //                                 />
+    //                             }
+    //                         />
+    //                     )
+    //                 )}
+    //                 {this.state.modalToShow === "Open modal" &&
+    //                     <Modal
+    //                         onClick={this.closeModal}
+    //                         header={this.state.modalObject.header}
+    //                         closeButton={this.state.modalObject.closeButton}
+    //                         text={this.state.modalObject.text}
+    //                         action={this.state.modalObject.action}
+    //                     />
+    //                 }
+    //             </div>
+    //         </>
+    //     )
+    // }
 }
 
 export default App;
