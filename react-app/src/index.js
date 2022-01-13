7e4a1eeedb66696cdc11f94b19245b9e85080f57
Anton Merkotun <antonmerkotun@gmail.com>
2022-01-13 22:45:01 +0200

    add redux in project

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
-import {compose, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import {rootReducer} from "./redux/reducers/rootReducer";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+// const store = createStore(rootReducer, applyMiddleware(thunk))
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+);
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
     <React.StrictMode>
